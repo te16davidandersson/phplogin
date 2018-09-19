9298ddb17b287dab271a4c23db685a75518dbc4e
anda16 <te16.david.andersson@umea.nti.se>
2018-09-19 09:22:18 +0200

    inte klar

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="se">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="widht=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+	<title>Jaha</title>
+	<style type="text/css">
+		main {
+			width: 80%;
+			margin: 0px auto;
+		}
+		p, h1, head {
+			font-family: 'Merriweather', serif;
+		}
+
+	</style>
+</head>
+<body>
+<main>
+	<h1>Du är ej välkommen här</h1>
+	<form action="login.php" method="POST">
+		<fieldset>
+			<legend>Logga in</legend>
+			<p>
+				<label for="username">Användarnamn: </label>
+				<input type="text" name="username" id="username">
+			</p>
+			<p>
+				<label for="password">Lösenord: </label>
+				<input type="password" name="password" id="password">
+			</p>
+			<p>
+				<input type="submit" name="submit" id="submit" value="Logga in">
+			</p>
+		</fieldset>
+	</form>
+</main>
+</body>
+</html>
